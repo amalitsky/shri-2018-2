899e4af5130bf23feed20c2f793fff7e2ecb8f51
Alex Malitsky <a.malitsky@gmail.com>
2018-10-30 21:50:14 +0300

    chor(TypeScript): Introduces typescript

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,9 @@ const concat = require('gulp-concat');
 const browserSync = require('browser-sync').create();
 const lessFiles = 'src/styles/**/*.less';
 const publish = require('gulp-gh-pages');
+const ts = require('gulp-typescript');
+
+const tsProject = ts.createProject('tsconfig.json');
 
 gulp.task('compile-css', function () {
     return gulp.src(lessFiles)
@@ -13,11 +16,22 @@ gulp.task('compile-css', function () {
         .pipe(browserSync.stream());
 });
 
-gulp.task('serve', ['compile-css'], function() {
+gulp.task('compile-ts', function () {
+  return tsProject.src()
+  .pipe(tsProject())
+  .js.pipe(gulp.dest('public'));
+});
+
+gulp.task('build', ['compile-css', 'compile-ts']);
+
+gulp.task('serve', ['build'], function() {
     browserSync.init({
         server: 'public',
         open: false
     });
+
+    gulp.watch('src/*.ts', ['compile-ts'])
+        .on('change', browserSync.reload);
 
     gulp.watch(lessFiles, ['compile-css']);
     gulp.watch('public/index.html')
@@ -26,7 +40,4 @@ gulp.task('serve', ['compile-css'], function() {
 
 gulp.task('default', ['serve']);
 
-gulp.task(
-    'publish',
-    () => gulp.src("./public/**/*").pipe(publish())
-);
+gulp.task('publish', () => gulp.src("./public/**/*").pipe(publish()));
--- src/app.ts
+++ src/app.ts
@@ -1,221 +1,287 @@
-"use strict";
-(function () {
+(function(): void {
     'use strict';
+
     /**
      * True when dragging action is active.
      */
-    let dragInProgress = false;
+    let dragInProgress: boolean = false;
+
     /**
      * Pointer id which kicked off the dragging action. Used only on switch from dragging to scaling.
      */
-    let dragPointerId = NaN;
+    let dragPointerId: number = NaN;
+
     /**
      * Previous pointer position during dragging.
      */
-    let dragPrevPosition = [
+    let dragPrevPosition: Coordinate = [
         NaN,
         NaN
     ];
+
     /**
      * True when scaling action is active.
      */
-    let scaleInProgress = false;
+    let scaleInProgress: boolean = false;
+
     /**
      * Value of the current image scale. Can be changed by scaling action.
      */
-    let currentScale = 1;
+    let currentScale: number = 1;
+
     /**
      * Two initial positions (reactive to the img element) of user's fingers. Set on
      * pointerdown event only and don't change during the scaling action.
      */
-    const scalingPreviousPos = [
+    const scalingPreviousPos: [Coordinate, Coordinate] = [
         [NaN, NaN],
         [NaN, NaN]
     ];
+
     /**
      * Pointer ids of the scaling action.
      */
-    const scalePointerIds = {
+    const scalePointerIds: {0: number, 1: number} = {
         0: NaN,
         1: NaN
     };
-    let viewport;
-    let vpWidth;
-    let vpHeight;
-    let vpTop;
-    let vpLeft;
-    let img;
-    let imgWidth;
-    let imgHeight;
+
+    let viewport: HTMLDivElement;
+    let vpWidth: number;
+    let vpHeight: number;
+    let vpTop: number;
+    let vpLeft: number;
+    let img: HTMLImageElement;
+    let imgWidth: number;
+    let imgHeight: number;
+
     /**
      * Current left and top margins of the image, set though 'transform: translate'.
      * @type {number[]}
      */
-    const imgPosition = [
+    const imgPosition: [number, number] = [
         0,
         0
     ];
+
     /**
      * Event handler for the viewport resizing event.
      */
-    function onWindowSizeChange() {
+    function onWindowSizeChange(): void {
         imgWidth = img.naturalWidth;
         imgHeight = img.naturalHeight;
+
         vpWidth = viewport.offsetWidth;
         vpHeight = viewport.offsetHeight;
+
         vpTop = viewport.offsetTop;
         vpLeft = viewport.offsetLeft;
     }
+
     /**
      * Calculates distance between two points.
      */
-    function getDistance(pointA, pointB) {
+    function getDistance(pointA: Coordinate, pointB: Coordinate): number {
         const dX = Math.abs(pointA[0] - pointB[0]);
         const dY = Math.abs(pointA[1] - pointB[1]);
+
         return Math.sqrt(Math.pow(dX, 2) + Math.pow(dY, 2));
     }
+
     /**
      * Coverts viewport coordinates to coordinates relative to img top left corner.
      */
-    function getImgRelativePos([clientX, clientY]) {
+    function getImgRelativePos([clientX, clientY]: Coordinate): Coordinate {
         const posX = (clientX - vpLeft - imgPosition[0]) / currentScale;
         const posY = (clientY - vpTop - imgPosition[1]) / currentScale;
+
         return [
             posX,
             posY
         ];
     }
+
     /**
      * Usually we have to move image by both axes after the scale change. This is due to the
      * fact that we want to keep image points initially picked by the user on `pointerdown` events
      * under his fingers while scaling the image.
      */
-    function getScalingShift([initX, initY], currentScale, scale) {
+    function getScalingShift(
+        [initX, initY]: Coordinate,
+        currentScale: number,
+        scale: number
+    ): Coordinate {
         const dX = -initX * (scale - currentScale);
         const dY = -initY * (scale - currentScale);
+
         return [dX, dY];
     }
+
     /**
      * Updates image margins set through `transform: translate`.
      */
-    function moveImage(dx, dy) {
+    function moveImage(dx: number, dy: number): void {
         const targetX = imgPosition[0] + dx;
         const targetY = imgPosition[1] + dy;
+
         setImgTransformation([targetX, targetY]);
     }
+
     /**
      * Applies actual transformations to the img element and updates global (sic!)
      * scope variables accordingly.
      */
-    function setImgTransformation(translate = imgPosition, scale = currentScale) {
+    function setImgTransformation(translate: Coordinate = imgPosition, scale: number = currentScale): void {
         const value = `translate(${translate[0]}px, ${translate[1]}px) scale(${scale})`;
+
         imgPosition[0] = translate[0];
         imgPosition[1] = translate[1];
+
         currentScale = scale;
+
         img.style.transform = value;
     }
+
     /**
      * Called to perform image scaling and moving.
      */
-    function performImgScaling(translationShift, scale) {
+    function performImgScaling(translationShift: Coordinate, scale: number): void {
         const translation = [
             imgPosition[0] + translationShift[0],
             imgPosition[1] + translationShift[1]
-        ];
+        ] as Coordinate;
+
         setImgTransformation(translation, scale);
     }
+
     /**
      * Pointer move event handler.
      */
-    function onPointerMove(e) {
-        const { clientX, clientY, pointerId } = e;
+    function onPointerMove(e: PointerEvent): void {
+        const {
+            clientX,
+            clientY,
+            pointerId
+        } = e;
+
         if (dragInProgress) {
             const [startX, startY] = dragPrevPosition;
             moveImage(clientX - startX, clientY - startY);
             dragPrevPosition = [clientX, clientY];
-        }
-        else if (scaleInProgress) {
+
+        } else if (scaleInProgress) {
             const index = pointerId === scalePointerIds[0] ? 0 : 1;
             const anchorIndex = index ? 0 : 1;
+
             const movingPointPrevPos = scalingPreviousPos[index];
             const anchorPos = scalingPreviousPos[anchorIndex];
             const movingPointPos = getImgRelativePos([clientX, clientY]);
-            const scaleChange = getDistance(anchorPos, movingPointPos) /
+
+            const scaleChange =
+                getDistance(anchorPos, movingPointPos) /
                 getDistance(anchorPos, movingPointPrevPos);
+
             const scale = currentScale * scaleChange;
-            performImgScaling(getScalingShift(anchorPos, currentScale, scale), scale);
+
+            performImgScaling(
+                getScalingShift(anchorPos, currentScale, scale),
+                scale
+            );
         }
     }
+
     /**
      * Pointer down event handler.
      * @param {MouseEvent} e
      */
-    function onPointerDown(e) {
-        const { clientX, clientY, pointerId } = e;
+    function onPointerDown(e: PointerEvent): void {
+        const {
+            clientX,
+            clientY,
+            pointerId
+        } = e;
+
         if (!dragInProgress) {
             dragInProgress = true;
             dragPrevPosition = [
                 clientX,
                 clientY
             ];
+
             dragPointerId = pointerId;
-        }
-        else {
+        } else {
             dragInProgress = false;
             scaleInProgress = true;
+
             scalePointerIds[0] = dragPointerId;
             scalingPreviousPos[0] = getImgRelativePos(dragPrevPosition);
+
             scalePointerIds[1] = pointerId;
             scalingPreviousPos[1] = getImgRelativePos([clientX, clientY]);
         }
     }
+
     /**
      * Resets internal app state to allow for the clean start once requested.
      */
-    function onPointerGone() {
+    function onPointerGone(): void {
         cancelDragging();
         cancelScaling();
     }
+
     /**
      * Cleans dragging action scope variables.
      */
-    function cancelDragging() {
+    function cancelDragging(): void {
         dragInProgress = false;
         dragPointerId = NaN;
         dragPrevPosition[0] = NaN;
         dragPrevPosition[1] = NaN;
     }
+
     /**
      * Cleans scaling action scope variables.
      */
-    function cancelScaling() {
+    function cancelScaling(): void {
         scaleInProgress = false;
+
         scalePointerIds[0] = NaN;
         scalePointerIds[1] = NaN;
+
         scalingPreviousPos[0] = [
             NaN,
             NaN
         ];
+
         scalingPreviousPos[1] = [
             NaN,
             NaN
         ];
     }
+
     /**
      * Sets up event handlers and runs some initialization functions.
      */
-    function init() {
-        viewport = document.querySelector('.window');
-        img = viewport.querySelector('img');
+    function init(): void {
+        viewport = document.querySelector('.window') as HTMLDivElement;
+        img = viewport.querySelector('img')!;
+
         onWindowSizeChange();
+
         // rendering image vertically and horizontally centered
-        moveImage(-(imgWidth - vpWidth) / 2, -(imgHeight - vpHeight) / 2);
+        moveImage(
+            -(imgWidth - vpWidth) / 2,
+            -(imgHeight - vpHeight) / 2
+        );
+
         viewport.addEventListener('pointermove', onPointerMove);
         viewport.addEventListener('pointerup', onPointerGone);
         viewport.addEventListener('pointerdown', onPointerDown);
         viewport.addEventListener('pointerleave', onPointerGone);
         viewport.addEventListener('pointercancel', onPointerGone);
     }
+
     window.addEventListener('load', init);
     window.addEventListener('resize', onWindowSizeChange);
 })();
+
